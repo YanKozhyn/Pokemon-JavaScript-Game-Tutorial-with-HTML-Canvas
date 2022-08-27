f2cdacf4164cc857831626b3aa73d1dac9f19dcc
JanHappy <yan.kozhyn@gmail.com>
2022-08-27 16:26:24 +0300

    player creation

--- main.js
+++ main.js
@@ -10,6 +10,20 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);
 const image = new Image();
 image.src = './assets/Images/Pellet Town.png';
 
+const playerImage = new Image();
+playerImage.src = './assets/Images/playerDown.png';
+
 image.onload = () => {
-  ctx.drawImage(image, -700, -550);
+  ctx.drawImage(image, -750, -600);
+  ctx.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 5,
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+  );
 };
